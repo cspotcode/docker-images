b3236a382ec0fee931dea2d4aba7f1eec04ff4d3
Andrew Bradley <cspotcode@gmail.com>
2020-03-20 21:09:15 -0400

    fix

--- .github/workflows/continuous.sh
+++ .github/workflows/continuous.sh
@@ -7,7 +7,7 @@ set -x
 for dir in $(ls images) ; do
     pushd images/$dir
     img=docker.pkg.github.com/cspotcode/docker-images/$dir:latest
-    docker login docker.pkg.github.com --username cspotcode
+    docker login docker.pkg.github.com --username cspotcode --password-stdin <<< "$GITHUB_TOKEN"
     docker build -t "$img" .
     docker push "$img"
 done
