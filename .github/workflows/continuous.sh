1fdbe2b9b91491eea65d3e9e0f6bd57705cbbb2d
Andrew Bradley <cspotcode@gmail.com>
2020-04-05 13:32:21 -0400

    fixes

--- .github/workflows/continuous.sh
+++ .github/workflows/continuous.sh
@@ -9,7 +9,10 @@ sudo ls -al /var/run/docker.sock
 sudo ls -al "$HOME"
 id
 
-sudo docker login docker.pkg.github.com --username cspotcode --password-stdin <<< "$GITHUB_TOKEN"
+sudo docker login docker.pkg.github.com --username "$GITHUB_ACTOR" --password-stdin <<< "$GITHUB_TOKEN"
+sudo docker pull alpine
+sudo docker tag alpine docker.pkg.github.com/cspotcode/docker-images/alpine-test-push:latest
+sudo docker push docker.pkg.github.com/cspotcode/docker-images/alpine-test-push:latest
 
 for dir in $(ls images) ; do
     pushd images/$dir
