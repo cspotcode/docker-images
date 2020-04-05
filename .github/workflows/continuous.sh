dcb2ea14dad8324c556f7f38cb0a86e99bfd5abb
Andrew Bradley <cspotcode@gmail.com>
2020-04-05 13:53:17 -0400

    fix

--- .github/workflows/continuous.sh
+++ .github/workflows/continuous.sh
@@ -3,24 +3,34 @@ set -euo pipefail
 shopt -s inherit_errexit
 
 set -x
+
+# Use our own home directory, not the one that Github Actions tries to give us
 export HOME="$CONTAINER_HOME"
 
-sudo ls -al /var/run/docker.sock
-sudo ls -al "$HOME"
-id
+# Using sudo for all docker stuff, because we need root to read/write the docker socket
 
+# Login to Github Packages
 sudo docker login docker.pkg.github.com --username "$GITHUB_ACTOR" --password-stdin <<< "$GITHUB_TOKEN"
+
+# Perform a test push of a small image to Github Actions to verify permission
 sudo docker pull alpine
 sudo docker tag alpine docker.pkg.github.com/cspotcode/docker-images/alpine-test-push:latest
 sudo docker push docker.pkg.github.com/cspotcode/docker-images/alpine-test-push:latest
 
-for dir in $(ls images) ; do
-    pushd images/$dir
+# Build and push a single image
+build_push() {
+    local dir
+    dir="$1"
+    pushd "images/$dir"
     # skip over windows docker images
     if [[ ! -f is-windows ]] ; then
         img=docker.pkg.github.com/cspotcode/docker-images/$dir:latest
         sudo docker build -t "$img" .
         sudo docker push "$img"
     fi
-    popd images/$dir
-done
+    popd
+}
+
+# Listed explicitly so we can control the ordering
+build_push node-ci
+build_push build-node-from-source
