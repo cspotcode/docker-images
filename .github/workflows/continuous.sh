418c778a3f6faa8bbb6f1501260bde835f715d03
Andrew Bradley <cspotcode@gmail.com>
2020-04-05 01:43:26 -0400

    fixes

--- .github/workflows/continuous.sh
+++ .github/workflows/continuous.sh
@@ -3,15 +3,21 @@ set -euo pipefail
 shopt -s inherit_errexit
 
 set -x
+export HOME="$CONTAINER_HOME"
 
 sudo ls -al /var/run/docker.sock
 sudo ls -al "$HOME"
 id
 
+sudo docker login docker.pkg.github.com --username cspotcode --password-stdin <<< "$GITHUB_TOKEN"
+
 for dir in $(ls images) ; do
     pushd images/$dir
-    img=docker.pkg.github.com/cspotcode/docker-images/$dir:latest
-    docker login docker.pkg.github.com --username cspotcode --password-stdin <<< "$GITHUB_TOKEN"
-    docker build -t "$img" .
-    docker push "$img"
+    # skip over windows docker images
+    if [[ ! -f is-windows ]] ; then
+        img=docker.pkg.github.com/cspotcode/docker-images/$dir:latest
+        sudo docker build -t "$img" .
+        sudo docker push "$img"
+    fi
+    popd images/$dir
 done
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 Docker images to be used in GitHub Actions and elsewhere.
+
+Windows containers are indicated via an empty `is-windows` file.
+
+Pushing windows images requires `--allow-nondistributable-artifacts` to be set in daemon options.
+See also: https://docs.docker.com/engine/reference/commandline/dockerd/
+https://medium.com/@amlys/solution-docker-push-fails-with-skipping-foreign-layer-a119e03bdf0d
+
+```
+"allow-nondistributable-artifacts": [
+  "docker.pkg.github.com:443"
+]
+```
